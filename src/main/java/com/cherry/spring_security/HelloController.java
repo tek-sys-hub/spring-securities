55a003bb411fb653b12d7148a3816f5213c1d293
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-20 22:47:58 +0545

    Spring Securities Setup

---
+++ src/main/java/com/cherry/spring_security/HelloController.java
@@ -0,0 +1,13 @@
+package com.cherry.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet (){
+        return "Welcome to cherry hub";
+    }
+}
