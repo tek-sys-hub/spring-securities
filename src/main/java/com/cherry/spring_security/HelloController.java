292ca706d1dffc254393c139433cf8cb16b3976b
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-23 18:18:36 +0545

    Added Session Id Request

--- src/main/java/com/cherry/spring_security/HelloController.java
+++ src/main/java/com/cherry/spring_security/HelloController.java
@@ -1,5 +1,6 @@
 package com.cherry.spring_security;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ public class HelloController {
 
     // Set the mapping for homepage
     @GetMapping("/")
-    public String greet (){
-        return "Welcome to cherry hub";
+    public String greet (HttpServletRequest request){
+        return "Welcome to cherry hub" + request.getSession().getId();
     }
 }
